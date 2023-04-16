5754159674c61af2cf62c94156dc1bc1be9b5b91
Max van den Hoven <max.hoven@gmail.com>
2023-04-16 21:33:01 +0200

    Fix scene asset name and start main script

---
+++ generate_images.py
@@ -0,0 +1,25 @@
+##########################################################################################
+# Imports
+##########################################################################################
+import sys
+import pathlib
+
+# Add base dir to PATH for module discovery within Blender
+base_dir = pathlib.Path(__file__).parent
+sys.path.append(str(base_dir))
+
+from blenderline.settings import ImageGenerationSettings
+
+
+##########################################################################################
+# Main script
+##########################################################################################
+def main() -> None:
+    settings = ImageGenerationSettings(base_dir, "config/example_beer/image.json")
+
+    scene_manager = settings.get_scene_manager()
+    scene_manager.initialize()
+
+
+if __name__ == "__main__":
+    main()
