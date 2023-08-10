07a2860783bd01a3d94c36bcaaad830817e609ae
Max van den Hoven <max.hoven@gmail.com>
2023-08-10 23:37:30 +0200

    Update setup requirements and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="blenderline",
-    version="0.1.5",
+    version="0.1.6",
     description="A pipeline for generating synthetic production line images",
     packages=find_packages(exclude=["examples*", "data*"]),
     long_description=long_description,
@@ -20,9 +20,11 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "numpy",
         "blender-stubs>=3.12.27",
         "bpy>=3.5.0",
+        "numpy",
+        "opencv-python",
+        "pyyaml",
     ],
     extras_require={
         "dev": [
@@ -30,9 +32,14 @@ setup(
             "isort>=5.12.0",
             "flake8>=6.0.0",
             "twine>=4.0.2",
+            "Sphinx==6.2.1",
+            "sphinx-autobuild==2021.3.14",
+            "furo==2023.5.20",
+            "sphinx-copybutton==0.5.2",
+            "sphinx_design==0.4.1",
         ],
     },
-    python_requires=">=3.10",
+    python_requires="==3.10.*",
     entry_points={
         "console_scripts": [
             "blenderline = blenderline.command_line:cli",
